210fe08eed206f9f6a3b3f01ea066b8b83b590f9
Mengkorng LY <mengkorng.ly@axivit.com>
2024-09-09 00:53:59 +0700

    (dev): created dev branch

--- lesson02/src/App.tsx
+++ lesson02/src/App.tsx
@@ -1,15 +1,15 @@
 import Chart from './components/Chart';
 import Navigation from './components/Navigation';
-import { Button } from './components/ui/button';
+import {Button} from './components/ui/button';
 
 function App() {
-  return (
-    <div className="flex flex-col items-center min-h-svh">
-      <Navigation />
-      {/* <Button variant={'default'}>Secondary</Button> */}
-      <Chart />
-    </div>
-  );
+    return (
+        <div className="flex flex-col items-center min-h-svh">
+            <Navigation/>
+            <Button variant={'default'}>Secondary</Button>
+            <Chart/>
+        </div>
+    );
 }
 
 export default App;
